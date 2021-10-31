d45ddf1a150ce0675a14805e0fde2382872c5614
bgfxc4 <bindermarc@gmx.de>
2021-10-31 11:24:10 +0100

    added template tree to choose existing item from

--- web-client/create-item/js/create.js
+++ web-client/create-item/js/create.js
@@ -1,4 +1,5 @@
 var categories = []
+var complete_equipment = []
 
 window.onload = function () {
 	$(".admin-only").css("visibility", "hidden")
@@ -12,10 +13,14 @@ window.onload = function () {
 		if (res[0] != undefined)
 			render_types(res[0].name)
 	})
+
+	request_equipment(res => {
+		complete_equipment = res
+		render_template_list(res)
+	})
 }
 
 function render_types(cat_name) {
-	console.log("Rendering: ", cat_name)
 	$('#create-item-type').html("")
 	for (var cat of categories) {
 		if (cat_name == cat.name) {
@@ -23,6 +28,51 @@ function render_types(cat_name) {
 				$('#create-item-type').append(`<option value="${t}">${t}</option>`)
 			return
 		}
+	}
+}
+
+function fill_in_item(item) {
+	$('#create-item-name').val(item.name + " - copy")
+	$('#create-item-description').val(item.description)
+	$('#create-item-storage').val(item.storage_place)
+	$('#create-item-category').val(item.category).change()
+	render_types(item.category)
+	$('#create-item-type').val(item.type).change()
+	$('#create-image-preview')[0].src = `data:image/jpeg;base64,${item.image}`
+}
+
+function render_template_list(equipment) {
+	var str = ""
+	for (var i = 0; i < equipment.length; i++) {
+		var c = ""
+		for (var j = 0; j < equipment[i].types.length; j++) {
+			var t = ""
+			for (var k = 0; k < equipment[i].types[j].equipment.length; k++) {
+				t += `<li><a onclick="fill_in_item(complete_equipment[${i}].types[${j}].equipment[${k}])" href="#" class="item-template">${equipment[i].types[j].equipment[k].name}</a></li>`
+			}
+			c += `<li><a href="#">${equipment[i].types[j].name}</a><ul>${t}</ul></li>`
+		}
+		str += `<li><a href="#">${equipment[i].name}</a><ul>${c}</ul></li>`
+	}
+	
+	$('#template-tree .tree').html(str)
+
+	var tree = document.querySelectorAll('ul.tree a:not(:last-child)');
+	for(var i = 0; i < tree.length; i++){
+		tree[i].addEventListener('click', function(e) {
+			var parent = e.target.parentElement;
+			var classList = parent.classList;
+			if(classList.contains("open")) {
+				classList.remove('open');
+				var opensubs = parent.querySelectorAll(':scope .open');
+				for(var i = 0; i < opensubs.length; i++){
+					opensubs[i].classList.remove('open');
+				}
+			} else {
+				classList.add('open');
+			}
+			e.preventDefault();
+		});
 	}
 }
 
