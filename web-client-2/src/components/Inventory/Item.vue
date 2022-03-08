a97d81b415fd2e0957f9f0a6c7bc149f991a4e71
bgfxc4 <bindermarc@gmx.de>
2022-03-08 23:45:59 +0100

    added qr and barcode generator

--- web-client-2/src/components/Inventory/Item.vue
+++ web-client-2/src/components/Inventory/Item.vue
@@ -2,7 +2,7 @@
 	<div id="item">
 		<div class="row container m-3">
 			<div class="col-5">
-				<img v-bind:src="$store.state.apiUrl + '/get-item-img/' + item.id" class="card-img" 
+				<img v-if="item.id" v-bind:src="$store.state.apiUrl + '/get-item-img/' + item.id" class="card-img" 
 									style="max-width: 30vw; max-height: 30vh; width: auto; height: auto; margin-left: 10px">
 			</div>
 			<div class="col-7">
@@ -12,14 +12,20 @@
 					<b>Storage:</b> {{ item.room }} - {{ item.shelf }} - {{ item.compartment }}<br>
 					<b>ID:</b> {{ item.id }}
 				</p>
+				<ShowQrBarCode :toShow="item.id"></ShowQrBarCode>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+	import ShowQrBarCode from "../ShowQrBarCode.vue"	
+
 	export default {
 		name: "Item",
+		components: {
+			ShowQrBarCode
+		},
 		data () {
 			return {
 				item: {},
@@ -33,6 +39,8 @@
 			this.typeName = this.$route.params.type
 			this.itemID = this.$route.params.itemID
 			this.$store.dispatch("getItemByID", {itemID: this.itemID, callback: answ => {
+				if (!answ)
+					return
 				console.log(answ.data[0].equipment[0])
 				this.item = answ.data[0].equipment[0]
 			}})
