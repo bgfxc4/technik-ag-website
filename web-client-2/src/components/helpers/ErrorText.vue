5a1da2e15d559ce9c37548543d8e0579b3c3a025
bgfxc4 <bindermarc@gmx.de>
2022-04-16 01:00:58 +0200

    added loginbutton if login credentials are wrong

--- web-client-2/src/components/helpers/ErrorText.vue
+++ web-client-2/src/components/helpers/ErrorText.vue
@@ -1,5 +1,11 @@
 <template>
-	<p class="text-danger h6"> {{ msgShow }}</p>
+	<div id="error-text">
+		<p class="text-danger h6"> {{ msgShow }}
+			<template v-if="msgIs401">
+				<br>You may need to <router-link to="/login" class="btn btn-sm btn-danger" >Log In</router-link>
+			</template>
+		</p>
+	</div>
 </template>
 
 <script>
@@ -14,7 +20,13 @@
 					return this.msg.response.data
 				else
 					return this.msg
-				}
+			},
+			msgIs401: function () {
+				if (this.msg.response?.status == 401)
+					return true
+				else
+					return false
+			}
 		}
 	}
 </script>
